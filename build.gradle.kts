36e3ef0e9cbfff1f7befa222260ae739e8c5178e
dev-jm <develop6013@gmail.com>
2025-05-07 20:39:53 +0900

    gradle 의존성 에러 해결 1. 시큐리티 부분 현재 필요 없어서 주석처리 2. DB 연결, JPA 부분 설정 파일에 에러가 떠서 현재 필요없으므로 주석처리

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	id("org.springframework.boot") version "3.4.5"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("com.vaadin") version "24.7.3"
+	id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "kr.plusb3b.games"
@@ -27,22 +28,22 @@ repositories {
 extra["vaadinVersion"] = "24.7.3"
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-hateoas")
-	implementation("org.springframework.boot:spring-boot-starter-security")
+	//implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+	//implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-web-services")
 	implementation("com.vaadin:vaadin-spring-boot-starter")
-	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
+	//implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("com.mysql:mysql-connector-j")
-	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+	//runtimeOnly("com.mysql:mysql-connector-j")
+	//runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
+	//testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 

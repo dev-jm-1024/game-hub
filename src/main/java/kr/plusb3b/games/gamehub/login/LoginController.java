44965103aaef18b080bfd0365a6e667a7ba760de
dev-jm <develop6013@gmail.com>
2025-05-07 21:07:59 +0900

    2025-05-07 20:40 Login 요청처리를 위해 LoginController 생성. V1

---
+++ src/main/java/kr/plusb3b/games/gamehub/login/LoginController.java
@@ -0,0 +1,10 @@
+package kr.plusb3b.games.gamehub.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/game-hub")
+public class LoginController {
+
+}

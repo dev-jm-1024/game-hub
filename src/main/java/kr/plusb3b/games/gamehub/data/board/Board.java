10a9b592d97d3f8337aac88a3c5f76a9d41c4e3b
dev-jm <develop6013@gmail.com>
2025-05-09 18:21:16 +0900

    2025-05-09 17:58 Board 테이블 도메인 클래스로 정의. lombok 인식못하는 이슈로 getter/setter, constructor, filed 선언. 추후 JPA를 위해 매개변수 없는 생성자로 선언

---
+++ src/main/java/kr/plusb3b/games/gamehub/data/board/Board.java
@@ -0,0 +1,120 @@
+package kr.plusb3b.games.gamehub.data.board;
+
+public class Board {
+
+    //고유 아이디
+    private Long board_id;
+
+    //게시판 이름 (공지사항, 자유게시판)
+    private String board_name;
+
+    //여분컬럼
+    private String board_extra1;
+    private String board_extra2;
+    private String board_extra3;
+    private String board_extra4;
+    private String board_extra5;
+    private String board_extra6;
+    private String board_extra7;
+    private String board_extra8;
+    private String board_extra9;
+    private String board_extra10;
+
+    public Board() {}
+
+    public Long getBoard_id() {
+        return board_id;
+    }
+
+    public void setBoard_id(Long board_id) {
+        this.board_id = board_id;
+    }
+
+    public String getBoard_name() {
+        return board_name;
+    }
+
+    public void setBoard_name(String board_name) {
+        this.board_name = board_name;
+    }
+
+    public String getBoard_extra1() {
+        return board_extra1;
+    }
+
+    public void setBoard_extra1(String board_extra1) {
+        this.board_extra1 = board_extra1;
+    }
+
+    public String getBoard_extra2() {
+        return board_extra2;
+    }
+
+    public void setBoard_extra2(String board_extra2) {
+        this.board_extra2 = board_extra2;
+    }
+
+    public String getBoard_extra3() {
+        return board_extra3;
+    }
+
+    public void setBoard_extra3(String board_extra3) {
+        this.board_extra3 = board_extra3;
+    }
+
+    public String getBoard_extra4() {
+        return board_extra4;
+    }
+
+    public void setBoard_extra4(String board_extra4) {
+        this.board_extra4 = board_extra4;
+    }
+
+    public String getBoard_extra5() {
+        return board_extra5;
+    }
+
+    public void setBoard_extra5(String board_extra5) {
+        this.board_extra5 = board_extra5;
+    }
+
+    public String getBoard_extra6() {
+        return board_extra6;
+    }
+
+    public void setBoard_extra6(String board_extra6) {
+        this.board_extra6 = board_extra6;
+    }
+
+    public String getBoard_extra7() {
+        return board_extra7;
+    }
+
+    public void setBoard_extra7(String board_extra7) {
+        this.board_extra7 = board_extra7;
+    }
+
+    public String getBoard_extra8() {
+        return board_extra8;
+    }
+
+    public void setBoard_extra8(String board_extra8) {
+        this.board_extra8 = board_extra8;
+    }
+
+    public String getBoard_extra9() {
+        return board_extra9;
+    }
+
+    public void setBoard_extra9(String board_extra9) {
+        this.board_extra9 = board_extra9;
+    }
+
+    public String getBoard_extra10() {
+        return board_extra10;
+    }
+
+    public void setBoard_extra10(String board_extra10) {
+        this.board_extra10 = board_extra10;
+    }
+}

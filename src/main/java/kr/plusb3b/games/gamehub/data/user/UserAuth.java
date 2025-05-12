b3d4238f531f219de3d8d8e753cca5d268abfa53
dev-jm <develop6013@gmail.com>
2025-05-12 11:13:57 +0900

    2025.05.12 v2: 리팩토링
    [UserAuth.java]
    1. 사용자 인증(UserAuth) 도메인 클래스 정의
       - 사용자 로그인 아이디(auth_user_id), 비밀번호, 마지막 로그인 시각 등 필드 구성
       - @Entity 및 @Table(name = "user_auth") 매핑
    
    2. 사용자(User)와의 1:1 관계 설정
       - @OneToOne + @MapsId + @JoinColumn(auth_mb_id): 외래키이자 PK 공유
       - User 엔티티에서 mappedBy로 양방향 연관관계 설정
    
    3. 식별자 공유 전략 적용
       - auth_mb_id = user.mb_id 구조로 설정하여 사용자 인증 정보를 동일 ID로 식별 가능
    
    공통:
    - 로그인 관련 핵심 정보 분리 및 보안 중심 구조 설계
    - 추후 Spring Security 연동 시 인증 정보로 활용 가능 (UserDetails 기반 확장 가능)

--- src/main/java/kr/plusb3b/games/gamehub/data/user/UserAuth.java
+++ src/main/java/kr/plusb3b/games/gamehub/data/user/UserAuth.java
@@ -1,12 +1,27 @@
 package kr.plusb3b.games.gamehub.data.user;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user_auth")
 public class UserAuth {
 
     //외래키
-    private Long mb_id;
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @MapsId // auth_mb_id = user.mb_id
+    @JoinColumn(name="auth_mb_id")
+    private User user;
+
     //고유키
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long auth_user_id;
 
     //사용자 비밀번호 추후 해시화 예정
@@ -16,6 +31,7 @@ public class UserAuth {
     private LocalDateTime auth_last_login;
 
     //여번컬럼
+    /*
     private String auth_extra1;
     private String auth_extra2;
     private String auth_extra3;
@@ -26,118 +42,7 @@ public class UserAuth {
     private String auth_extra8;
     private String auth_extra9;
     private String auth_extra10;
-
+    */
     public UserAuth(){}
 
-    public Long getMb_id() {
-        return mb_id;
-    }
-
-    public void setMb_id(Long mb_id) {
-        this.mb_id = mb_id;
-    }
-
-    public Long getAuth_user_id() {
-        return auth_user_id;
-    }
-
-    public void setAuth_user_id(Long auth_user_id) {
-        this.auth_user_id = auth_user_id;
-    }
-
-    public String getAuth_password() {
-        return auth_password;
-    }
-
-    public void setAuth_password(String auth_password) {
-        this.auth_password = auth_password;
-    }
-
-    public LocalDateTime getAuth_last_login() {
-        return auth_last_login;
-    }
-
-    public void setAuth_last_login(LocalDateTime auth_last_login) {
-        this.auth_last_login = auth_last_login;
-    }
-
-    public String getAuth_extra1() {
-        return auth_extra1;
-    }
-
-    public void setAuth_extra1(String auth_extra1) {
-        this.auth_extra1 = auth_extra1;
-    }
-
-    public String getAuth_extra2() {
-        return auth_extra2;
-    }
-
-    public void setAuth_extra2(String auth_extra2) {
-        this.auth_extra2 = auth_extra2;
-    }
-
-    public String getAuth_extra3() {
-        return auth_extra3;
-    }
-
-    public void setAuth_extra3(String auth_extra3) {
-        this.auth_extra3 = auth_extra3;
-    }
-
-    public String getAuth_extra4() {
-        return auth_extra4;
-    }
-
-    public void setAuth_extra4(String auth_extra4) {
-        this.auth_extra4 = auth_extra4;
-    }
-
-    public String getAuth_extra5() {
-        return auth_extra5;
-    }
-
-    public void setAuth_extra5(String auth_extra5) {
-        this.auth_extra5 = auth_extra5;
-    }
-
-    public String getAuth_extra6() {
-        return auth_extra6;
-    }
-
-    public void setAuth_extra6(String auth_extra6) {
-        this.auth_extra6 = auth_extra6;
-    }
-
-    public String getAuth_extra7() {
-        return auth_extra7;
-    }
-
-    public void setAuth_extra7(String auth_extra7) {
-        this.auth_extra7 = auth_extra7;
-    }
-
-    public String getAuth_extra8() {
-        return auth_extra8;
-    }
-
-    public void setAuth_extra8(String auth_extra8) {
-        this.auth_extra8 = auth_extra8;
-    }
-
-    public String getAuth_extra9() {
-        return auth_extra9;
-    }
-
-    public void setAuth_extra9(String auth_extra9) {
-        this.auth_extra9 = auth_extra9;
-    }
-
-    public String getAuth_extra10() {
-        return auth_extra10;
-    }
-
-    public void setAuth_extra10(String auth_extra10) {
-        this.auth_extra10 = auth_extra10;
-    }
 }

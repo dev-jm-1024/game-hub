50d4252fe5b8a3eaabfa8c700bc81a129eb3a98c
dev-jm <develop6013@gmail.com>
2025-05-12 11:13:01 +0900

    2025.05.12 v2: 리팩토링
    [User.java]
    1. 사용자(User) 도메인 클래스 정의
       - 사용자 고유 ID(mb_id), 닉네임, 프로필 이미지, 상태 메시지, 가입일, 신고 횟수 등 필드 구성
       - @Entity 및 @Table(name = "user") 매핑
    
    2. 게시글, 댓글, 점수, 인증정보 등 연관관계 설정
       - @OneToMany(mappedBy = "user") → Posts, Comments, UserScore
       - @OneToOne(mappedBy = "user") → UserAuth, UserLoginInfo, UserPrivate
    
    3. 사용자 권한 필드(enum Role) 추가
       - USER, ADMIN, MODERATOR 등 역할 정의
       - @Enumerated(EnumType.STRING)으로 문자열 기반 DB 저장
       - 생성자에 기본값 설정 또는 서비스단에서 할당 가능
    
    4. cascade = ALL + orphanRemoval = true 적용
       - 사용자 삭제 시 연관된 게시글, 댓글, 로그인 정보, 점수 등 일괄 삭제 처리 가능
    
    공통:
    - 사용자 중심 커뮤니티 기능을 위한 핵심 엔티티
    - 권한 기반 인가 처리 및 정보 보호(1:1 민감정보 분리) 구조 반영

--- src/main/java/kr/plusb3b/games/gamehub/data/user/User.java
+++ src/main/java/kr/plusb3b/games/gamehub/data/user/User.java
@@ -1,19 +1,63 @@
 package kr.plusb3b.games.gamehub.data.user;
 
+import jakarta.persistence.*;
+import kr.plusb3b.games.gamehub.data.board.Comments;
+import kr.plusb3b.games.gamehub.data.board.Posts;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
-//@Entity
-//@Table(name = "user")
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long mb_id;
+
     private String mb_nickname;
     private String mb_profile_url;
     private String mb_status_message;
     private LocalDateTime mb_join_date;
     private int mb_act;
+
+    public enum Role {
+        USER, ADMIN, MODERATOR
+    }
+    @Enumerated(EnumType.STRING)
+    @Column(name = "mb_role")
+    private Role mb_role;
+
     private int mb_report_cnt;
 
+
+
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Posts> mb_posts = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comments> comments = new ArrayList<Comments>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserScore> user_scores = new ArrayList<>();
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private UserAuth user_auth;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private UserLoginInfo user_login_info;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private UserPrivate user_private;
+
     //여분컬럼1~10
+    /*
     private String mb_extra1;
     private String mb_extra2;
     private String mb_extra3;
@@ -24,144 +68,8 @@ public class User {
     private String mb_extra8;
     private String mb_extra9;
     private String mb_extra10;
-
+    */
     public User () {}
 
-    public Long getMb_id() {
-        return mb_id;
-    }
 
-    public void setMb_id(Long mb_id) {
-        this.mb_id = mb_id;
-    }
-
-    public String getMb_nickname() {
-        return mb_nickname;
-    }
-
-    public void setMb_nickname(String mb_nickname) {
-        this.mb_nickname = mb_nickname;
-    }
-
-    public String getMb_profile_url() {
-        return mb_profile_url;
-    }
-
-    public void setMb_profile_url(String mb_profile_url) {
-        this.mb_profile_url = mb_profile_url;
-    }
-
-    public String getMb_status_message() {
-        return mb_status_message;
-    }
-
-    public void setMb_status_message(String mb_status_message) {
-        this.mb_status_message = mb_status_message;
-    }
-
-    public LocalDateTime getMb_join_date() {
-        return mb_join_date;
-    }
-
-    public void setMb_join_date(LocalDateTime mb_join_date) {
-        this.mb_join_date = mb_join_date;
-    }
-
-    public int getMb_act() {
-        return mb_act;
-    }
-
-    public void setMb_act(int mb_act) {
-        this.mb_act = mb_act;
-    }
-
-    public int getMb_report_cnt() {
-        return mb_report_cnt;
-    }
-
-    public void setMb_report_cnt(int mb_report_cnt) {
-        this.mb_report_cnt = mb_report_cnt;
-    }
-
-
-
-    public String getMb_extra1() {
-        return mb_extra1;
-    }
-
-    public void setMb_extra1(String mb_extra1) {
-        this.mb_extra1 = mb_extra1;
-    }
-
-    public String getMb_extra2() {
-        return mb_extra2;
-    }
-
-    public void setMb_extra2(String mb_extra2) {
-        this.mb_extra2 = mb_extra2;
-    }
-
-    public String getMb_extra3() {
-        return mb_extra3;
-    }
-
-    public void setMb_extra3(String mb_extra3) {
-        this.mb_extra3 = mb_extra3;
-    }
-
-    public String getMb_extra4() {
-        return mb_extra4;
-    }
-
-    public void setMb_extra4(String mb_extra4) {
-        this.mb_extra4 = mb_extra4;
-    }
-
-    public String getMb_extra5() {
-        return mb_extra5;
-    }
-
-    public void setMb_extra5(String mb_extra5) {
-        this.mb_extra5 = mb_extra5;
-    }
-
-    public String getMb_extra6() {
-        return mb_extra6;
-    }
-
-    public void setMb_extra6(String mb_extra6) {
-        this.mb_extra6 = mb_extra6;
-    }
-
-    public String getMb_extra7() {
-        return mb_extra7;
-    }
-
-    public void setMb_extra7(String mb_extra7) {
-        this.mb_extra7 = mb_extra7;
-    }
-
-    public String getMb_extra8() {
-        return mb_extra8;
-    }
-
-    public void setMb_extra8(String mb_extra8) {
-        this.mb_extra8 = mb_extra8;
-    }
-
-    public String getMb_extra9() {
-        return mb_extra9;
-    }
-
-    public void setMb_extra9(String mb_extra9) {
-        this.mb_extra9 = mb_extra9;
-    }
-
-    public String getMb_extra10() {
-        return mb_extra10;
-    }
-
-    public void setMb_extra10(String mb_extra10) {
-        this.mb_extra10 = mb_extra10;
-    }
 }

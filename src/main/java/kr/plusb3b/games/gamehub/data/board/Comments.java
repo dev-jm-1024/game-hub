28d681cabcefe0ebe2e836019ee9ec353626f953
dev-jm <develop6013@gmail.com>
2025-05-12 11:08:30 +0900

    2025.05.12 v2: 리팩토링
    [Comments.java]
    1. 댓글(Comment) 도메인 클래스 정의
       - 댓글 고유 ID, 댓글 내용, 좋아요/싫어요/신고 횟수 필드 구성
       - @Entity 및 @Table(name = "comments") 매핑
    
    2. 게시글(Posts) 및 사용자(User)와의 연관관계 설정
       - @ManyToOne 및 @JoinColumn(post_id, mb_id) 적용
       - 댓글 → 게시글, 댓글 → 사용자 양방향 매핑 가능
    
    공통:
    - 댓글 엔티티를 통해 게시글과 유저 간 커뮤니케이션 기능 구축 기반 마련
    - 추후 cascade 및 orphanRemoval로 생명주기 관리 가능

--- src/main/java/kr/plusb3b/games/gamehub/data/board/Comments.java
+++ src/main/java/kr/plusb3b/games/gamehub/data/board/Comments.java
@@ -1,15 +1,31 @@
 package kr.plusb3b.games.gamehub.data.board;
 
+
+import jakarta.persistence.*;
+import kr.plusb3b.games.gamehub.data.user.User;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="comments")
 public class Comments {
 
     //댓글 고유 아이디
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long comment_id;
 
     //어떤 게시글의 댓글인지? 외래키임
-    private Long post_id;
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Posts posts;
 
     //작성자 아이디 (내부 식별 아이디. 로그인 아이디 아님)
-    private Long mb_id;
+    @ManyToOne
+    @JoinColumn(name="mb_id")
+    private User user;
 
     //댓글 내용
     private String comment_content;
@@ -21,6 +37,7 @@ public class Comments {
     private int report_count;
 
     //여분컬럼
+    /*
     private String comm_extra1;
     private String comm_extra2;
     private String comm_extra3;
@@ -31,142 +48,9 @@ public class Comments {
     private String comm_extra8;
     private String comm_extra9;
     private String comm_extra10;
+     */
 
     public Comments() {}
 
-    public Long getComment_id() {
-        return comment_id;
-    }
 
-    public void setComment_id(Long comment_id) {
-        this.comment_id = comment_id;
-    }
-
-    public Long getPost_id() {
-        return post_id;
-    }
-
-    public void setPost_id(Long post_id) {
-        this.post_id = post_id;
-    }
-
-    public Long getMb_id() {
-        return mb_id;
-    }
-
-    public void setMb_id(Long mb_id) {
-        this.mb_id = mb_id;
-    }
-
-    public String getComment_content() {
-        return comment_content;
-    }
-
-    public void setComment_content(String comment_content) {
-        this.comment_content = comment_content;
-    }
-
-    public int getLike_count() {
-        return like_count;
-    }
-
-    public void setLike_count(int like_count) {
-        this.like_count = like_count;
-    }
-
-    public int getDislike_count() {
-        return dislike_count;
-    }
-
-    public void setDislike_count(int dislike_count) {
-        this.dislike_count = dislike_count;
-    }
-
-    public int getReport_count() {
-        return report_count;
-    }
-
-    public void setReport_count(int report_count) {
-        this.report_count = report_count;
-    }
-
-    public String getComm_extra1() {
-        return comm_extra1;
-    }
-
-    public void setComm_extra1(String comm_extra1) {
-        this.comm_extra1 = comm_extra1;
-    }
-
-    public String getComm_extra2() {
-        return comm_extra2;
-    }
-
-    public void setComm_extra2(String comm_extra2) {
-        this.comm_extra2 = comm_extra2;
-    }
-
-    public String getComm_extra3() {
-        return comm_extra3;
-    }
-
-    public void setComm_extra3(String comm_extra3) {
-        this.comm_extra3 = comm_extra3;
-    }
-
-    public String getComm_extra4() {
-        return comm_extra4;
-    }
-
-    public void setComm_extra4(String comm_extra4) {
-        this.comm_extra4 = comm_extra4;
-    }
-
-    public String getComm_extra5() {
-        return comm_extra5;
-    }
-
-    public void setComm_extra5(String comm_extra5) {
-        this.comm_extra5 = comm_extra5;
-    }
-
-    public String getComm_extra6() {
-        return comm_extra6;
-    }
-
-    public void setComm_extra6(String comm_extra6) {
-        this.comm_extra6 = comm_extra6;
-    }
-
-    public String getComm_extra7() {
-        return comm_extra7;
-    }
-
-    public void setComm_extra7(String comm_extra7) {
-        this.comm_extra7 = comm_extra7;
-    }
-
-    public String getComm_extra8() {
-        return comm_extra8;
-    }
-
-    public void setComm_extra8(String comm_extra8) {
-        this.comm_extra8 = comm_extra8;
-    }
-
-    public String getComm_extra9() {
-        return comm_extra9;
-    }
-
-    public void setComm_extra9(String comm_extra9) {
-        this.comm_extra9 = comm_extra9;
-    }
-
-    public String getComm_extra10() {
-        return comm_extra10;
-    }
-
-    public void setComm_extra10(String comm_extra10) {
-        this.comm_extra10 = comm_extra10;
-    }
 }

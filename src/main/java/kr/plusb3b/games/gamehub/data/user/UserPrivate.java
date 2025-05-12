491d10a88eff129dcc6cf8a1a170e19c749e2fdc
dev-jm <develop6013@gmail.com>
2025-05-12 11:21:13 +0900

    2025.05.12 v2: 리팩토링
    [UserPrivate.java]
    1. 사용자 민감 정보(UserPrivate) 도메인 클래스 정의
       - 이메일(pri_email), 생년월일(pri_birth), 성별(pri_gender) 필드 구성
       - @Entity 및 @Table(name = "user_private") 매핑
    
    2. 사용자(User)와의 1:1 식별자 공유 관계 설정
       - @OneToOne + @MapsId + @JoinColumn(pri_mb_id) 구성
       - User의 기본키(mb_id)를 공유하여 민감 정보와 사용자 정보의 정합성 확보
    
    3. 민감 정보 분리 설계 적용
       - 개인정보 보호를 위해 별도 테이블로 분리
       - User 엔티티와 명확한 경계 유지하며 보안성 강화
    
    공통:
    - 이메일, 생년월일 등 개인정보를 분리 저장함으로써 보안 및 컴플라이언스 요건 대응
    - 1:1 구조 기반의 데이터 접근 제어 구현 기반 마련

--- src/main/java/kr/plusb3b/games/gamehub/data/user/UserPrivate.java
+++ src/main/java/kr/plusb3b/games/gamehub/data/user/UserPrivate.java
@@ -1,11 +1,29 @@
 package kr.plusb3b.games.gamehub.data.user;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.atmosphere.config.service.Get;
+
 import java.time.LocalDateTime;
 
+
+@Entity
+@Getter
+@Setter
+@Table(name="user_private")
 public class UserPrivate {
 
-    //외래키
-    private Long mb_id;
+    // PK이자 FK인 필드
+    @Id
+    @Column(name = "pri_mb_id")
+    private Long priMbId;
+
+    // User와 1:1 매핑, 외래키 + PK 공유
+    @OneToOne
+    @MapsId // priMbId = user.mb_id (식별자 공유)
+    @JoinColumn(name = "pri_mb_id")
+    private User user;
 
     //사용자 이메일
     private String pri_email;
@@ -15,6 +33,7 @@ public class UserPrivate {
     private String pri_gender;
 
     //여분컬럼 1~10
+    /*
     private String pri_extra1;
     private String pri_extra2;
     private String pri_extra3;
@@ -25,118 +44,7 @@ public class UserPrivate {
     private String pri_extra8;
     private String pri_extra9;
     private String pri_extra10;
+    */
 
     public UserPrivate() {}
-
-    public Long getMb_id() {
-        return mb_id;
-    }
-
-    public void setMb_id(Long mb_id) {
-        this.mb_id = mb_id;
-    }
-
-    public String getPri_email() {
-        return pri_email;
-    }
-
-    public void setPri_email(String pri_email) {
-        this.pri_email = pri_email;
-    }
-
-    public LocalDateTime getPri_birth() {
-        return pri_birth;
-    }
-
-    public void setPri_birth(LocalDateTime pri_birth) {
-        this.pri_birth = pri_birth;
-    }
-
-    public String getPri_gender() {
-        return pri_gender;
-    }
-
-    public void setPri_gender(String pri_gender) {
-        this.pri_gender = pri_gender;
-    }
-
-    public String getPri_extra1() {
-        return pri_extra1;
-    }
-
-    public void setPri_extra1(String pri_extra1) {
-        this.pri_extra1 = pri_extra1;
-    }
-
-    public String getPri_extra2() {
-        return pri_extra2;
-    }
-
-    public void setPri_extra2(String pri_extra2) {
-        this.pri_extra2 = pri_extra2;
-    }
-
-    public String getPri_extra3() {
-        return pri_extra3;
-    }
-
-    public void setPri_extra3(String pri_extra3) {
-        this.pri_extra3 = pri_extra3;
-    }
-
-    public String getPri_extra4() {
-        return pri_extra4;
-    }
-
-    public void setPri_extra4(String pri_extra4) {
-        this.pri_extra4 = pri_extra4;
-    }
-
-    public String getPri_extra5() {
-        return pri_extra5;
-    }
-
-    public void setPri_extra5(String pri_extra5) {
-        this.pri_extra5 = pri_extra5;
-    }
-
-    public String getPri_extra6() {
-        return pri_extra6;
-    }
-
-    public void setPri_extra6(String pri_extra6) {
-        this.pri_extra6 = pri_extra6;
-    }
-
-    public String getPri_extra7() {
-        return pri_extra7;
-    }
-
-    public void setPri_extra7(String pri_extra7) {
-        this.pri_extra7 = pri_extra7;
-    }
-
-    public String getPri_extra8() {
-        return pri_extra8;
-    }
-
-    public void setPri_extra8(String pri_extra8) {
-        this.pri_extra8 = pri_extra8;
-    }
-
-    public String getPri_extra9() {
-        return pri_extra9;
-    }
-
-    public void setPri_extra9(String pri_extra9) {
-        this.pri_extra9 = pri_extra9;
-    }
-
-    public String getPri_extra10() {
-        return pri_extra10;
-    }
-
-    public void setPri_extra10(String pri_extra10) {
-        this.pri_extra10 = pri_extra10;
-    }
 }

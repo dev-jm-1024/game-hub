20bd35ce7f84d091ea63f5d7430d87bc8b4e0a4b
dev-jm <develop6013@gmail.com>
2025-05-13 12:17:49 +0900

    2025-05-13 12:17
    AdminController 생성
    요청처리 작업 코드만 작성

---
+++ src/main/java/kr/plusb3b/games/gamehub/admin/AdminController.java
@@ -0,0 +1,16 @@
+package kr.plusb3b.games.gamehub.admin;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping
+    public String showAdminPage(){
+        return "admin";
+    }
+}

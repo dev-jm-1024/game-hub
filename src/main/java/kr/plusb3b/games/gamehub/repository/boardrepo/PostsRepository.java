8ee74e13f8480d4790fef92aac5cb0c2d8455886
dev-jm-1024 <develop6013@gmail.com>
2025-05-16 02:04:38 +0900

    25.05.16 02:03
    findByBoard_Board_idAndPost_id 메소드
    : 게시판 아이디와 게시물 아이디 입력받아
    게시물 데이터 가져오기

--- src/main/java/kr/plusb3b/games/gamehub/repository/boardrepo/PostsRepository.java
+++ src/main/java/kr/plusb3b/games/gamehub/repository/boardrepo/PostsRepository.java
@@ -6,9 +6,16 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
     List<Posts> findByBoardId(Long boardId, Pageable pageable);
+
+    //List로 Posts 데이터 받기
+    //해당 게시판의 아이디와 게시물의 아이디를 입력받아 해당 게시물 데이터 가져오기
+    //예외처리를 보다 쉽게 처리하기 위해 Optional 로 처리
+    Optional<Posts> findByBoard_Board_idAndPost_id(Long boardId, Long postId);
+
 
 }

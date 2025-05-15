728cbb90c61d36e8d5e1e5a4a08083702d4a1dcd
dev-jm-1024 <develop6013@gmail.com>
2025-05-15 16:43:37 +0900

    25.05.15 16:38
    main-board 페이지에서 게시물을 최대 5개 까지 보여줘야하므로 Pageable 추가
    
    Jparepository는 페이징, 정렬 등 가능

--- src/main/java/kr/plusb3b/games/gamehub/repository/boardrepo/PostsRepository.java
+++ src/main/java/kr/plusb3b/games/gamehub/repository/boardrepo/PostsRepository.java
@@ -2,13 +2,13 @@ package kr.plusb3b.games.gamehub.repository.boardrepo;
 
 import kr.plusb3b.games.gamehub.api.dto.board.Board;
 import kr.plusb3b.games.gamehub.api.dto.board.Posts;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
-    List<Posts> findAllByBoard(Board board);
-
+    List<Posts> findByBoardId(Long boardId, Pageable pageable);
 
 }

ba3d9095eac7266ae201cd476ab6a2205bb603ff
dev-jm <develop6013@gmail.com>
2025-05-15 14:36:06 +0900

    2025-05-15 14:32
    게시물 가져오는 메소드 생성

--- src/main/java/kr/plusb3b/games/gamehub/repository/boardrepo/PostsRepository.java
+++ src/main/java/kr/plusb3b/games/gamehub/repository/boardrepo/PostsRepository.java
@@ -1,7 +1,14 @@
 package kr.plusb3b.games.gamehub.repository.boardrepo;
 
+import kr.plusb3b.games.gamehub.api.dto.board.Board;
 import kr.plusb3b.games.gamehub.api.dto.board.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    List<Posts> findAllByBoard(Board board);
+
+
 }

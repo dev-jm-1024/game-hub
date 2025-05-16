209f5e1f146b28643cd18832c712aff5a4595639
dev-jm <develop6013@gmail.com>
2025-05-16 22:07:09 +0900

    2025-05-16 22:03
    로그인 페이지 URI 경로 작성
    View 반환

--- src/main/java/kr/plusb3b/games/gamehub/api/controller/login/LoginController.java
+++ src/main/java/kr/plusb3b/games/gamehub/api/controller/login/LoginController.java
@@ -1,10 +1,17 @@
 package kr.plusb3b.games.gamehub.api.controller.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/game-hub")
 public class LoginController {
+
+    //로그인 페이지로 이동
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login-form";
+    }
 
 }

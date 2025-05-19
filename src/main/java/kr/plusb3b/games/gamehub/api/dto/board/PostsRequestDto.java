af741ab105f8d411d853bfda1f0f22b883c57bf0
dev-jm <develop6013@gmail.com>
2025-05-19 16:14:21 +0900

    2025-05-19 16:07
    변수들 카멜방식으로 변경

--- src/main/java/kr/plusb3b/games/gamehub/api/dto/board/PostsRequestDto.java
+++ src/main/java/kr/plusb3b/games/gamehub/api/dto/board/PostsRequestDto.java
@@ -3,7 +3,6 @@ package kr.plusb3b.games.gamehub.api.dto.board;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
@@ -11,17 +10,17 @@ public class PostsRequestDto
 {
     //유효성 검사
     @NotBlank(message="제목을 입력해야합니다.")
-    private String post_title;
+    private String postTitle;
 
     //유효성 검사
     @NotBlank(message="내용을 입력하세요.")
-    private String post_content;
+    private String postContent;
 
     public PostsRequestDto(){}
 
     public PostsRequestDto(String post_title, String post_content) {
-        this.post_title = post_title;
-        this.post_content = post_content;
+        this.postTitle = post_title;
+        this.postContent = post_content;
     }
 
 

75575da20f69c3aac2798e35531a41d74653cbc1
dev-jm <develop6013@gmail.com>
2025-05-19 16:12:51 +0900

    2025-05-19 16:07
    로그인한 아이디로 User 객체 메소드를 UserAuthRepository에서 UserRepository로 옮김
    
    또한 변수들도 카멜방식으로 변경

--- src/main/java/kr/plusb3b/games/gamehub/repository/userrepo/UserRepository.java
+++ src/main/java/kr/plusb3b/games/gamehub/repository/userrepo/UserRepository.java
@@ -3,6 +3,19 @@ package kr.plusb3b.games.gamehub.repository.userrepo;
 
 import kr.plusb3b.games.gamehub.api.dto.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT ua.user FROM UserAuth ua WHERE ua.authUserId = :authUserId")
+    User findByAuthUserId(@Param("authUserId") String authUserId);
+
+    // 로그인 ID(authUserId)는 UserAuth에 있지만, 연결된 User를 통해 가져올 수 있도록 메서드 작성
+    Optional<User> findByMbNickname(String mbNickname);
+
+    // 또는 username이 로그인 ID(authUserId)라면 아래와 같이 UserAuth 연동용 쿼리도 가능
+    Optional<User> findByUserAuth_AuthUserId(String authUserId);
 }

e400e490ebd43c185ee9f774eff0c2a68dfb1b69
dev-jm <develop6013@gmail.com>
2025-05-19 16:11:46 +0900

    2025-05-19 16:07
    쿼리 자동생성으로 인한 @Query어노테이션 제거와, 변수들을 카멜방식으로 변경

--- src/main/java/kr/plusb3b/games/gamehub/repository/userrepo/UserAuthRepository.java
+++ src/main/java/kr/plusb3b/games/gamehub/repository/userrepo/UserAuthRepository.java
@@ -1,6 +1,5 @@
 package kr.plusb3b.games.gamehub.repository.userrepo;
 
-import kr.plusb3b.games.gamehub.api.dto.user.User;
 import kr.plusb3b.games.gamehub.api.dto.user.UserAuth;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,14 +9,10 @@ import java.util.Optional;
 
 public interface UserAuthRepository extends JpaRepository<UserAuth, Long> {
 
-    //로그인한 아이디를 바탕으로 사용자의 고유 아이디를 가져와야함. 그게 mb_id임
-    @Query("SELECT auth_user_id FROM UserAuth WHERE UserAuth.auth_user_id = :loginId")
-    User findUserByLoginId(@Param("loginId") String loginId);
+    // Spring JPA 메서드 규칙에 따라 필드명을 그대로 메서드명에 사용 (쿼리 자동 생성됨)
+    Optional<UserAuth> findByAuthUserId(String authUserId);
 
-    //사용자의 해시화된 패스워드와 로그인한 아이디를 통해 객체가 존재하는 지 판단
-    @Query("SELECT UserAuth FROM UserAuth WHERE UserAuth.auth_user_id = :userId AND UserAuth.auth_password = :uwerPw")
-    Optional<UserAuth> findByUserAuth(@Param("userId") String userId, @Param("userPw") String userPw);
-
-    // 사용자 아이디로만 찾기 (비밀번호는 따로 matches() 처리)
-    Optional<UserAuth> findByAuth_user_id(String auth_user_id);
+    // 사용자 아이디 + 패스워드로 조회 (로그인용)
+    @Query("SELECT ua FROM UserAuth ua WHERE ua.authUserId = :userId AND ua.authPassword = :authPassword")
+    Optional<UserAuth> findByUserAuth(@Param("userId") String authUserId, @Param("authPassword") String authPassword);
 }

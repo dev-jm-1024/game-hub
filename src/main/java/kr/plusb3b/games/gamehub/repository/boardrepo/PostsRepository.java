cb2b41f457f7dd9d18b075d9700bfc4624b6f685
dev-jm <develop6013@gmail.com>
2025-05-19 16:10:58 +0900

    2025-05-19 16:07
    JPA에서 제대로 판단할 수 있게 메소드 이름 변경과 boardId가 String 으로 변경하여서 Long에서 String 으로 수정

--- src/main/java/kr/plusb3b/games/gamehub/repository/boardrepo/PostsRepository.java
+++ src/main/java/kr/plusb3b/games/gamehub/repository/boardrepo/PostsRepository.java
@@ -4,18 +4,22 @@ import kr.plusb3b.games.gamehub.api.dto.board.Board;
 import kr.plusb3b.games.gamehub.api.dto.board.Posts;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
-    List<Posts> findByBoardId(Long boardId, Pageable pageable);
+    List<Posts> findByBoard_BoardId(String boardId, Pageable pageable);
+
 
     //List로 Posts 데이터 받기
     //해당 게시판의 아이디와 게시물의 아이디를 입력받아 해당 게시물 데이터 가져오기
     //예외처리를 보다 쉽게 처리하기 위해 Optional 로 처리
-    Optional<Posts> fingByBoardIdAndPostsId(Long boardId, Long postId);
+    Optional<Posts> findByBoard_BoardIdAndPostId(String boardId, Long postId);
+
 
 
 }

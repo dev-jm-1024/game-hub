10d10cae1f30d331b83bef58f9399d0e75e5de37
dev-jm <develop6013@gmail.com>
2025-05-19 16:15:22 +0900

    2025-05-19 16:07
    - UserAuthRepository에서 UserRepository로 변경
    - 카멜방식으로 바꿈에 따라 getter/setter 메소드명 변경되어서 이에 맞게 변경

--- src/main/java/kr/plusb3b/games/gamehub/api/controller/board/rest/RestBoardController.java
+++ src/main/java/kr/plusb3b/games/gamehub/api/controller/board/rest/RestBoardController.java
@@ -7,10 +7,12 @@ import kr.plusb3b.games.gamehub.api.dto.board.PostFiles;
 import kr.plusb3b.games.gamehub.api.dto.board.Posts;
 import kr.plusb3b.games.gamehub.api.dto.board.PostsRequestDto;
 import kr.plusb3b.games.gamehub.api.dto.user.User;
+import kr.plusb3b.games.gamehub.api.dto.user.UserAuth;
 import kr.plusb3b.games.gamehub.repository.boardrepo.BoardRepository;
 import kr.plusb3b.games.gamehub.repository.boardrepo.PostFilesRepository;
 import kr.plusb3b.games.gamehub.repository.boardrepo.PostsRepository;
 import kr.plusb3b.games.gamehub.repository.userrepo.UserAuthRepository;
+import kr.plusb3b.games.gamehub.repository.userrepo.UserRepository;
 import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.http.HttpStatus;
@@ -34,14 +36,15 @@ public class RestBoardController {
 
     private final PostsRepository postsRepo;
     private final PostFilesRepository postFilesRepo;
-    private final UserAuthRepository userAuthRepo;
+    private final UserRepository userRepo;
     private final BoardRepository boardRepo;
 
     public RestBoardController(PostsRepository postsRepo, PostFilesRepository postFilesRepo,
-                               UserAuthRepository userAuthRepo, BoardRepository boardRepo) {
+                               UserAuthRepository userAuthRepo, BoardRepository boardRepo,
+                               UserRepository userRepo) {
         this.postsRepo = postsRepo;
         this.postFilesRepo = postFilesRepo;
-        this.userAuthRepo = userAuthRepo;
+        this.userRepo = userRepo;
         this.boardRepo = boardRepo;
     }
 
@@ -50,17 +53,20 @@ public class RestBoardController {
     @PostMapping("/api/v1/{boardId}/new")
     @ResponseStatus(HttpStatus.CREATED)
     public Posts insertPosts(@Valid @RequestBody PostsRequestDto postsRequestDto,
-                             @PathVariable Long board_id,  @AuthenticationPrincipal UserDetails userDetails) {
+                             @PathVariable String boardId,  @AuthenticationPrincipal UserDetails userDetails) {
 
-        String loginId = userDetails.getUsername(); // 예: "jaeminlee123"
-        User user = userAuthRepo.findUserByLoginId(loginId); // 여기서 mb_id 포함된 user 객체 가져옴
+        String authUserId = userDetails.getUsername(); // 예: "jaeminlee123"
+        User user = userRepo.findByAuthUserId(authUserId);
+
+
+
 
         // 게시판 엔티티 조회
-        Board board = boardRepo.findById(board_id)
+        Board board = boardRepo.findById(boardId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시판이 존재하지 않습니다."));
 
-        String title = postsRequestDto.getPost_title();
-        String content = postsRequestDto.getPost_content();
+        String title = postsRequestDto.getPostTitle();
+        String content = postsRequestDto.getPostContent();
         int viewCount = 0;
         LocalDateTime createdAt = LocalDateTime.now();
         LocalDateTime updatedAt = null;
@@ -70,12 +76,12 @@ public class RestBoardController {
         Posts post = new Posts();
         post.setUser(user); //사용자의 고유 아이디인 mb_id
         post.setBoard(board); //게시판 아이디
-        post.setPost_title(postsRequestDto.getPost_title());
-        post.setPost_content(postsRequestDto.getPost_content());
-        post.setView_count(0);
-        post.setCreated_at(LocalDateTime.now());
-        post.setUpdated_at(LocalDateTime.now());
-        post.setPost_act(1);
+        post.setPostTitle(postsRequestDto.getPostTitle());
+        post.setPostContent(postsRequestDto.getPostContent());
+        post.setViewCount(0);
+        post.setCreatedAt(LocalDateTime.now());
+        post.setUpdatedAt(LocalDateTime.now());
+        post.setPostAct(1);
 
         return postsRepo.save(post);
     }
@@ -89,8 +95,8 @@ public class RestBoardController {
 
     //화면에 게시물 데이터 뿌려주기
     @GetMapping("/api/v1/{boardId}/{postId}/view")
-    public Posts getPosts(@PathVariable Long boardId, @PathVariable Long postId) {
-        return postsRepo.fingByBoardIdAndPostsId(boardId, postId)
+    public Posts getPosts(@PathVariable String boardId, @PathVariable Long postId) {
+        return postsRepo.findByBoard_BoardIdAndPostId(boardId, postId)
                 .orElseThrow(() -> new ResponseStatusException(
                         HttpStatus.NOT_FOUND, "해당 게시글을 찾을 수 없습니다."
                 ));

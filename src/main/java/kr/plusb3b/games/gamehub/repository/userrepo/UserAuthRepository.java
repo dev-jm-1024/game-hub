2217a6770c2eb0e61a75713eaca7ae8648d057ea
dev-jm <develop6013@gmail.com>
2025-05-22 01:18:24 +0900

    2025.05.22 01:14
    UserAuthRepository
    사용자아이디 + 사용자 패스워드 조합으로 사용자 찾는 메소드 제거.
    
    평문 비밀번호로 조회하면 당연히 결과가 없어서 무용지물

--- src/main/java/kr/plusb3b/games/gamehub/repository/userrepo/UserAuthRepository.java
+++ src/main/java/kr/plusb3b/games/gamehub/repository/userrepo/UserAuthRepository.java
@@ -12,7 +12,4 @@ public interface UserAuthRepository extends JpaRepository<UserAuth, Long> {
     // Spring JPA 메서드 규칙에 따라 필드명을 그대로 메서드명에 사용 (쿼리 자동 생성됨)
     Optional<UserAuth> findByAuthUserId(String authUserId);
 
-    // 사용자 아이디 + 패스워드로 조회 (로그인용)
-    @Query("SELECT ua FROM UserAuth ua WHERE ua.authUserId = :userId AND ua.authPassword = :authPassword")
-    Optional<UserAuth> findByUserAuth(@Param("userId") String authUserId, @Param("authPassword") String authPassword);
 }

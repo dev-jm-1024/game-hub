44be7552435a4c05694fae15fa159ce2bdb3daa3
dev-jm <develop6013@gmail.com>
2025-05-24 22:13:28 +0900

    2025.05.24 22:06
    1.LocalDateTime 에서 LocalDate로 변경
    2. Valid 검사 하기 -- NotBlank

--- src/main/java/kr/plusb3b/games/gamehub/api/dto/board/Posts.java
+++ src/main/java/kr/plusb3b/games/gamehub/api/dto/board/Posts.java
@@ -1,10 +1,12 @@
 package kr.plusb3b.games.gamehub.api.dto.board;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import kr.plusb3b.games.gamehub.api.dto.user.User;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 
@@ -26,11 +28,15 @@ public class Posts {
     @JoinColumn(name = "mbId")
     private User user; // 작성자 외래키
 
+    @NotBlank(message = "제목을 입력하세요")
     private String postTitle;
+
+    @NotBlank(message = "내용을 입력하세요.")
     private String postContent; //게시물 내용
+
     private int viewCount; //조회수
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
     private int postAct;
 
     public Posts() {}

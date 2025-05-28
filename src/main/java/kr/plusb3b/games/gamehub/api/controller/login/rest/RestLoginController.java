8879077743a4d8d754ee90df107df22a36b505e0
dev-jm <develop6013@gmail.com>
2025-05-29 00:49:38 +0900

    RestLoginController 25.05.29  00:44
    수정사항 및 개편사항
    
    1. 회원의 계정이 비활성화되어있는 지 검사하는 로직 추가
    
    2. 계정이 활성화 되어있는 경우, 로그인 정보(접속 시각, 접속 IP 주소)를 JPA를 통해 삽입
    
    3. 클라이언트의 IP얻는 메소드 생성

--- src/main/java/kr/plusb3b/games/gamehub/api/controller/login/rest/RestLoginController.java
+++ src/main/java/kr/plusb3b/games/gamehub/api/controller/login/rest/RestLoginController.java
@@ -1,10 +1,16 @@
 package kr.plusb3b.games.gamehub.api.controller.login.rest;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import kr.plusb3b.games.gamehub.api.dto.user.User;
 import kr.plusb3b.games.gamehub.api.dto.user.UserAuth;
+import kr.plusb3b.games.gamehub.api.dto.user.UserLoginInfo;
 import kr.plusb3b.games.gamehub.api.jwt.JwtProvider;
 import kr.plusb3b.games.gamehub.repository.userrepo.UserAuthRepository;
+import kr.plusb3b.games.gamehub.repository.userrepo.UserLoginInfoRepository;
+import kr.plusb3b.games.gamehub.repository.userrepo.UserRepository;
 import kr.plusb3b.games.gamehub.security.SecurityConfig;
+import kr.plusb3b.games.gamehub.security.SnowflakeIdGenerator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpHeaders;
@@ -13,69 +19,69 @@ import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import jakarta.servlet.http.HttpServletRequest;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/game-hub/api/v1/login")
 public class RestLoginController {
 
+    private final UserRepository userRepository;
     @Value("${app.api.version}")
     private String appApiVersion;
 
     private final UserAuthRepository userAuthRepo;
-
+    private final UserLoginInfoRepository loginInfoRepo;
     private final PasswordEncoder passwordEncoder;
     private final JwtProvider jwtProvider;
 
+
+
     public RestLoginController(UserAuthRepository userAuthRepo,
-                               PasswordEncoder passwordEncoder, JwtProvider jwtProvider) {
+                               PasswordEncoder passwordEncoder, JwtProvider jwtProvider,
+                               UserLoginInfoRepository loginInfoRepo, UserRepository userRepository) {
         this.userAuthRepo = userAuthRepo;
         this.passwordEncoder = passwordEncoder;
         this.jwtProvider = jwtProvider;
+        this.loginInfoRepo = loginInfoRepo;
+        this.userRepository = userRepository;
     }
-
-    //로그인 체크
-//    @PostMapping
-//    public ResponseEntity checkLogin(@ModelAttribute  UserAuth userAuth){
-//        String id = userAuth.getAuth_user_id();
-//        String hashPw = passwordEncoder.encode(userAuth.getAuth_password());
-//
-//        Optional<UserAuth> userAuthOptional = userAuthRepo.findByUserAuth(id, hashPw);
-//
-//        try{
-//            if(userAuthOptional.isPresent()){
-//                return ResponseEntity.ok(userAuthOptional.get());
-//            }else{
-//                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-//            }
-//        }catch (Exception e){
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-//        }
-//    }
 
     //로그인 체크
     @PostMapping
     public ResponseEntity<?> checkLogin(@RequestParam("authUserId") String authUserId,
                                         @RequestParam("authPassword") String authPassword,
-                                        HttpServletResponse response) {
+                                        HttpServletResponse response, HttpServletRequest request) {
 
         //Optional<UserAuth> userOptional = userAuthRepo.findByAuthUserId(userAuth.getAuthUserId());
 
         boolean isAuthUserId = authUserId != null && !authUserId.isEmpty();
         boolean isAuthPassword = authPassword != null && !authPassword.isEmpty();
 
-        if (isAuthUserId && isAuthPassword) {
+        //이때 로그인 시도하려는 아이디의 값이 mbAct=0 이면 회원 탈퇴로 판단
+        int isMemberBreak = userRepository.findMbActByAuthUserId(authUserId);
 
+        if(isMemberBreak == 0) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("탈퇴한 회원 입니다.");
+        }
+
+        if (isAuthUserId && isAuthPassword)
+        {
+
+            //로그인한 아이디로 회원이 존재하는 지 확인
             Optional<UserAuth> userAuthOptional = userAuthRepo.findByAuthUserId(authUserId);
 
-            if(userAuthOptional.isPresent()) {
+            if(userAuthOptional.isPresent())
+            {
                 UserAuth userAuth = userAuthOptional.get();
 
-                if (passwordEncoder.matches(authPassword, userAuth.getAuthPassword())) {
+                if (passwordEncoder.matches(authPassword, userAuth.getAuthPassword()))
+                {
                     String token = jwtProvider.createToken(userAuth.getAuthUserId());
 
-                    // ✅ 쿠키로 JWT 내려주기
+                    //쿠키로 JWT 내려주기
                     ResponseCookie cookie = ResponseCookie.from("jwt", token)
                             .httpOnly(true)
                             .secure(false) // ✅ 개발 시 false, 운영 시 true
@@ -84,10 +90,42 @@ public class RestLoginController {
                             .maxAge(60 * 60) // 1시간
                             .build();
 
-                    // ✅ 브라우저에 쿠키를 내려주는 핵심 한 줄!
+                    //브라우저에 쿠키를 내려줌
                     response.setHeader(HttpHeaders.SET_COOKIE, cookie.toString());
 
+                    //로그인 기록 고유 아이디 생성
+                    SnowflakeIdGenerator snowflakeIdGenerator = new SnowflakeIdGenerator(0,0);
+                    Long loginInfoId = snowflakeIdGenerator.nextId();
 
+                    //로그인 시각 생성
+                    LocalDateTime loginTime = LocalDateTime.now();
+
+                    //접속 IP주소 획득
+                    String ipAddress = getClientIP(request);
+
+                    //로그인 한 아이디로 사용자 고유 아이디 찾기
+                    UserLoginInfo userLoginInfo = new UserLoginInfo();
+                    Optional<User> userOptional = userRepository.findByUserAuth_AuthUserId(authUserId);
+                    if(userOptional.isPresent()){
+                        User user = userOptional.get();
+
+                        //UserLoginInfo 조립 시작
+                        userLoginInfo.setUser(user);
+                        userLoginInfo.setLoginInfoId(loginInfoId);
+                        userLoginInfo.setLoginTime(loginTime);
+                        userLoginInfo.setIpAddress(ipAddress);
+                    }
+
+
+                    try{
+                        //로그인 기록 저장
+                        loginInfoRepo.save(userLoginInfo);
+
+                        //UserAuth의 authLastLogin 기록 업데이트
+                        userAuthRepo.updateLastLoginByUserId(loginTime, authUserId);
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
 
                     return ResponseEntity.ok()
                             .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
@@ -101,5 +139,27 @@ public class RestLoginController {
 
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 실패");
     }
+
+    //클라이언트의 로그인한 IP얻는 메소드
+    public String getClientIP(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
 
 }

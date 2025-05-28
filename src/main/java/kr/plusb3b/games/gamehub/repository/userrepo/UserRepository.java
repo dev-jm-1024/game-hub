6a29a178c2bfbf27ecfa230305b5e3fe187e4dc6
dev-jm <develop6013@gmail.com>
2025-05-29 01:11:19 +0900

    UserRepository 25.05.29  00:44
    수정사항 및 개편사항
    
    1. 로그인한 아이디를 통해 회원의 계정 상태 찾는 메소드 추가
    
    2. 회원 탈퇴 메소드 추가

--- src/main/java/kr/plusb3b/games/gamehub/repository/userrepo/UserRepository.java
+++ src/main/java/kr/plusb3b/games/gamehub/repository/userrepo/UserRepository.java
@@ -3,6 +3,7 @@ package kr.plusb3b.games.gamehub.repository.userrepo;
 
 import kr.plusb3b.games.gamehub.api.dto.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -18,4 +19,13 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     // 또는 username이 로그인 ID(authUserId)라면 아래와 같이 UserAuth 연동용 쿼리도 가능
     Optional<User> findByUserAuth_AuthUserId(String authUserId);
+
+    @Query("SELECT u.mbAct FROM User u WHERE u.userAuth.authUserId = :authUserId")
+    int findMbActByAuthUserId(@Param("authUserId") String authUserId);
+
+    //회원계정 비활성화 -- 탈퇴
+    @Modifying
+    @Query("UPDATE User SET mbAct = 0 WHERE userAuth.authUserId = :authUserId AND mbAct = 1")
+    int updateMbActByAuthUserId(@Param("authUserId") String authUserId);
+
 }

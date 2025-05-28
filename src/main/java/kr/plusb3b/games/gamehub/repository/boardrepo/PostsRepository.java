3d7d5d837682b0c5958b03e65151420345abe476
dev-jm <develop6013@gmail.com>
2025-05-28 15:25:08 +0900

    PostsRepository 25.05.28 15:13
    수정사항 및 개편사항
    
    1. posts 내용 업데이트하는 메소드 추가
    2. posts 비활성화로 바꾸는 메소드 추가
    
    둘 다 update문임

--- src/main/java/kr/plusb3b/games/gamehub/repository/boardrepo/PostsRepository.java
+++ src/main/java/kr/plusb3b/games/gamehub/repository/boardrepo/PostsRepository.java
@@ -4,9 +4,12 @@ import kr.plusb3b.games.gamehub.api.dto.board.Board;
 import kr.plusb3b.games.gamehub.api.dto.board.Posts;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import jakarta.transaction.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,6 +25,23 @@ public interface PostsRepository extends JpaRepository<Posts, Long> {
     //boardId를 입력받아, 해당 게시판의 게시물이 존재하는 지 확인
     List<Posts> findByBoard_BoardId(String boardId);
 
+    //posts 내용 업데이트
+    @Modifying
+    @Transactional
+    @Query("UPDATE Posts SET postTitle = :postTitle, postContent = :postContent, updatedAt = :updatedAt WHERE postId = :postId AND board.boardId = :boardId")
+    int updatePostsByPostIdAndBoardId(
+            @Param("postTitle") String postTitle,
+            @Param("postContent") String postContent,
+            @Param("updatedAt") LocalDate updatedAt,
+            @Param("postId") Long postId,
+            @Param("boardId") String boardId
+    );
+
+    //postAct = 0 으로 바꿔줌
+    @Modifying
+    @Transactional
+    @Query("UPDATE Posts SET postAct = 0 WHERE postId = :postId")
+    int deletePostsByPostId(@Param("postId") Long postId);
 
 
 }

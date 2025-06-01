4854000c112969bbe8a9689760857287a557f1c2
dev-jm-1024 <develop6013@gmail.com>
2025-06-01 15:23:38 +0900

    RestBoardController 25.06.01 15:16
    주석제거

--- src/main/java/kr/plusb3b/games/gamehub/api/controller/board/rest/RestBoardController.java
+++ src/main/java/kr/plusb3b/games/gamehub/api/controller/board/rest/RestBoardController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 
 @RestController("RestBoardController")
-//@RequestMapping(path = "/board")
 @RequestMapping(path="/api/v1/board") // 경로 수정 ver
 public class RestBoardController {
 

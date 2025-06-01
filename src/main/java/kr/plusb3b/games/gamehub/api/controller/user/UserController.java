1e3e4638e590e20a0a08972880f55cad8b7a82e7
dev-jm-1024 <develop6013@gmail.com>
2025-06-01 15:34:47 +0900

    UserController 프로필 업데이트 요청처리

--- src/main/java/kr/plusb3b/games/gamehub/api/controller/user/UserController.java
+++ src/main/java/kr/plusb3b/games/gamehub/api/controller/user/UserController.java
@@ -1,42 +1,58 @@
 package kr.plusb3b.games.gamehub.api.controller.user;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import kr.plusb3b.games.gamehub.api.dto.user.RequestUserUpdateDto;
+import kr.plusb3b.games.gamehub.api.dto.user.User;
+import kr.plusb3b.games.gamehub.api.dto.user.UserAuth;
+import kr.plusb3b.games.gamehub.api.dto.user.UserPrivate;
+import kr.plusb3b.games.gamehub.repository.userrepo.UserAuthRepository;
+import kr.plusb3b.games.gamehub.repository.userrepo.UserPrivateRepository;
+import kr.plusb3b.games.gamehub.repository.userrepo.UserRepository;
+import kr.plusb3b.games.gamehub.security.AccessControlService;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
-@RequestMapping("/mypage")
+@RequestMapping("/my-page")
 public class UserController {
 
+    private final AccessControlService access;
+    private final UserRepository userRepo;
+    private final UserPrivateRepository userPrivateRepo;
+    private final UserAuthRepository userAuthRepo;
 
-    @GetMapping
-    public String showUserSettings() {
-        return "usersetting";
+    public UserController(AccessControlService access, UserRepository userRepo,
+                          UserPrivateRepository userPrivateRepo, UserAuthRepository userAuthRepo) {
+        this.access = access;
+        this.userRepo = userRepo;
+        this.userPrivateRepo = userPrivateRepo;
+        this.userAuthRepo = userAuthRepo;
     }
 
-    @GetMapping("/game") //내 게임 페이지 안내
-    public String showMyGamePage() {
-        return "game";
-    }
 
-    @GetMapping("/profile") //프로필 페이지 안내
-    public String showMyProfilePage() {
-        return "profile";
-    }
+    //프로필 변경 페이지 요청 처리
+    @GetMapping("/{mbId}/edit-profile")
+    public String showEditProfilePage(@PathVariable("mbId") Long mbId, HttpServletRequest request, Model model) {
 
-    @GetMapping("/favorites")//즐겨찾기 페이지 안내
-    public String showFavoritesPage() {
-        return "favorites";
-    }
+        User user = access.getAuthenticatedUser(request);
+        Long memberId = user.getMbId();
 
-    @GetMapping("/myboard") //내가 쓴 글 확인하기
-    public String showMyBoardPage() {
-        return "myboard";
-    }
+        Optional<User> userOptional = userRepo.findById(memberId);
+        Optional<UserAuth> userauthOptional = userAuthRepo.findAllByUserId(memberId);
+        Optional<UserPrivate> userprivateOptional = userPrivateRepo.findUserPrivateByMbId(memberId);
 
-    @GetMapping("/mycomment") //내가 쓴 댓글 확인하기
-    public String showMyCommentPage() {
-        return "mycomment";
+        userOptional.ifPresent(value -> model.addAttribute("user", value));
+        userauthOptional.ifPresent(value -> model.addAttribute("userAuth", value));
+        userprivateOptional.ifPresent(value -> model.addAttribute("userPrivate", value));
+
+
+        return "profile/profile-edit-form";
     }
 }

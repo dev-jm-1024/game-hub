759ad675e9eaa65a8fa16ba862c0bbd84b317002
dev-jm-1024 <develop6013@gmail.com>
2025-06-06 01:21:17 +0900

    UserRepository
    25.06.06
    
    @Transactional 추가

--- src/main/java/kr/plusb3b/games/gamehub/repository/userrepo/UserRepository.java
+++ src/main/java/kr/plusb3b/games/gamehub/repository/userrepo/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -16,11 +17,13 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserAuth_AuthUserId(@Param("authUserId") String authUserId);
 
     //회원계정 비활성화 -- 탈퇴
+    @Transactional
     @Modifying
     @Query("UPDATE User SET mbAct = 0 WHERE userAuth.authUserId = :authUserId AND mbAct = 1")
     int updateMbActByAuthUserId(@Param("authUserId") String authUserId);
 
     //프로필 업데이트 메소드
+    @Transactional
     @Modifying
     @Query("UPDATE User u SET u.mbNickname = :mbNickName, u.mbProfileUrl = :mbProfileUrl, u.mbStatusMessage = :mbStatusMessage WHERE u.mbId = :mbId")
     int updateUserByMbId(

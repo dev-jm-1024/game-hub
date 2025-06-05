ecd74d12eb28ae54b5c982ac76893b4acb745639
dev-jm-1024 <develop6013@gmail.com>
2025-06-06 01:17:58 +0900

    RequestUserUpdateDto
    25.06.06
    
    비밀번호 필드 제거
    : 별도의 페이지 제작 예정
    
    equals(), hashCode() 오버라이드

--- src/main/java/kr/plusb3b/games/gamehub/api/dto/user/RequestUserUpdateDto.java
+++ src/main/java/kr/plusb3b/games/gamehub/api/dto/user/RequestUserUpdateDto.java
@@ -4,13 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Getter
 @Setter
 public class RequestUserUpdateDto {
 
     private String authUserId; //로그인 아이디
-    private String authPassword; //로그인 비밀번호
+//    private String authPassword; //로그인 비밀번호
     private String priEmail; //이메일
     private LocalDate priBirth; //사용자 생일
     private String mbNickName; //사용자 닉네임
@@ -20,14 +21,26 @@ public class RequestUserUpdateDto {
 
     public RequestUserUpdateDto() {}
 
-    public RequestUserUpdateDto(String authUserId, String authPassword, String priEmail, LocalDate priBirth,
-                                String mbNickName, String mbStatusMessage, String priGender) {
+    public RequestUserUpdateDto(String authUserId, String priEmail, LocalDate priBirth, String mbNickName,
+                                String mbStatusMessage, String priGender, String mbProfileUrl) {
         this.authUserId = authUserId;
-        this.authPassword = authPassword;
         this.priEmail = priEmail;
         this.priBirth = priBirth;
         this.mbNickName = mbNickName;
         this.mbStatusMessage = mbStatusMessage;
         this.priGender = priGender;
+        this.mbProfileUrl = mbProfileUrl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        RequestUserUpdateDto that = (RequestUserUpdateDto) o;
+        return Objects.equals(authUserId, that.authUserId) && Objects.equals(priEmail, that.priEmail) && Objects.equals(priBirth, that.priBirth) && Objects.equals(mbNickName, that.mbNickName) && Objects.equals(mbStatusMessage, that.mbStatusMessage) && Objects.equals(priGender, that.priGender) && Objects.equals(mbProfileUrl, that.mbProfileUrl);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(authUserId, priEmail, priBirth, mbNickName, mbStatusMessage, priGender, mbProfileUrl);
     }
 }

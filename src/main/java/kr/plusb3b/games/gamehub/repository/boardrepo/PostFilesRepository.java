a4b3d0c802b3b753a40cb3f18bfdfe26c648d99c
dev-jm-1024 <develop6013@gmail.com>
2025-06-18 01:20:05 +0900

    PostFilesRepository
    findPostFilesByPost_PostId메소드 추가
    : postId 입력 받아서 첨부파일 찾음

--- src/main/java/kr/plusb3b/games/gamehub/repository/boardrepo/PostFilesRepository.java
+++ src/main/java/kr/plusb3b/games/gamehub/repository/boardrepo/PostFilesRepository.java
@@ -3,5 +3,9 @@ package kr.plusb3b.games.gamehub.repository.boardrepo;
 import kr.plusb3b.games.gamehub.api.dto.board.PostFiles;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PostFilesRepository extends JpaRepository<PostFiles, Long> {
+
+    Optional<PostFiles> findPostFilesByPost_PostId(Long postPostId);
 }

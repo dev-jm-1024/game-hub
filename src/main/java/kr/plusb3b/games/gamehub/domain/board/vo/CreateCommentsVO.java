0bb7a6e116e3f50083bb40186957484cb5030e70
dev-jm <develop6013@gmail.com>
2025-07-15 01:01:10 +0900

    VO작성 : insert 할 때 디폴트

---
+++ src/main/java/kr/plusb3b/games/gamehub/domain/board/vo/CreateCommentsVO.java
@@ -0,0 +1,15 @@
+package kr.plusb3b.games.gamehub.domain.board.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CreateCommentsVO {
+
+    private final int commentAct = 1;
+    private final int dislikeCount = 0;
+    private final int likeCount = 0;
+    private final int reportCount = 0;
+
+}

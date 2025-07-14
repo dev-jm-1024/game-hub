463f18c76b5c4ac11c4b7fd50be01a7732f6a0ec
dev-jm <develop6013@gmail.com>
2025-07-14 16:46:34 +0900

    RequestCommentDto - refact
    : 생성자를 어노테이션을 사용하여 간결화
    : 댓글 내용 Null값 비허용 어노테이션 추가

--- src/main/java/kr/plusb3b/games/gamehub/domain/board/dto/RequestCommentDto.java
+++ src/main/java/kr/plusb3b/games/gamehub/domain/board/dto/RequestCommentDto.java
@@ -1,21 +1,20 @@
 package kr.plusb3b.games.gamehub.domain.board.dto;
 
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
+@AllArgsConstructor
 public class RequestCommentDto {
 
+    @NotNull
     private String commentContent;
     private String boardId;
     private Long postId;
 
     public RequestCommentDto() {}
 
-    public RequestCommentDto(String commentContent, String boardId) {
-        this.commentContent = commentContent;
-        this.boardId = boardId;
-    }
 }

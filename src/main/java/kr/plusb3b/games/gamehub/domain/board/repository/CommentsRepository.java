7a79cfc52b5cf36e10a93d2ba68e85ad53e79b99
dev-jm <develop6013@gmail.com>
2025-07-14 16:44:22 +0900

    CommentsRepository - refact
    댓글 가져오는 쿼리 메소드 작성
    : postId 와 댓글 상태1인 댓글만 가져오게

--- src/main/java/kr/plusb3b/games/gamehub/domain/board/repository/CommentsRepository.java
+++ src/main/java/kr/plusb3b/games/gamehub/domain/board/repository/CommentsRepository.java
@@ -4,5 +4,11 @@ package kr.plusb3b.games.gamehub.domain.board.repository;
 import kr.plusb3b.games.gamehub.domain.board.entity.Comments;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentsRepository extends JpaRepository<Comments, Long> {
+
+
+    // interface CommentsRepository
+    List<Comments> findByPosts_PostIdAndCommentActOrderByCreatedAtDesc(Long postId, int commentAct);
 }

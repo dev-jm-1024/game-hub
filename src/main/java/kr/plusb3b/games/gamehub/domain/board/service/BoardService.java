5f7d31f11ecb0ad3da780e27272ab5817a96b50f
dev-jm <develop6013@gmail.com>
2025-07-14 16:41:02 +0900

    BoardService - refact
    1. loadTop5LatesPostsByBoard()
    : key 타입을 String에서 Board로 변경

--- src/main/java/kr/plusb3b/games/gamehub/domain/board/service/BoardService.java
+++ src/main/java/kr/plusb3b/games/gamehub/domain/board/service/BoardService.java
@@ -1,5 +1,6 @@
 package kr.plusb3b.games.gamehub.domain.board.service;
 
+import kr.plusb3b.games.gamehub.domain.board.entity.Board;
 import kr.plusb3b.games.gamehub.domain.board.entity.Posts;
 
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.Map;
 public interface BoardService {
 
     //게시판과 각 게시판의 게시물 가져오기 -- 최신순 5개 : 내림차순 정렬
-    Map<String, List<Posts>> loadTop5LatestPostsByBoard();
+    Map<Board, List<Posts>> loadTop5LatestPostsByBoard();
 
     //게시판의 이름 변경
     void renameBoard(String oldName, String newName);

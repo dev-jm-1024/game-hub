f06dd6b38ef24c3eca9bccbf8a53e1d82d6a7b11
dev-jm <develop6013@gmail.com>
2025-07-14 16:52:38 +0900

    CommentService - create
    : 댓글 서비스 추상화 메소드 작성

---
+++ src/main/java/kr/plusb3b/games/gamehub/domain/board/service/CommentService.java
@@ -0,0 +1,16 @@
+package kr.plusb3b.games.gamehub.domain.board.service;
+
+import kr.plusb3b.games.gamehub.domain.board.dto.RequestCommentDto;
+import kr.plusb3b.games.gamehub.domain.board.entity.Comments;
+import kr.plusb3b.games.gamehub.domain.board.vo.CreateCommentsVO;
+import kr.plusb3b.games.gamehub.domain.user.entity.User;
+
+import java.util.List;
+
+public interface CommentService {
+
+    boolean createComment(CreateCommentsVO cvo, RequestCommentDto requestCommentDto, User user);
+
+    List<Comments> getComments(String boardId, Long postId);
+
+}

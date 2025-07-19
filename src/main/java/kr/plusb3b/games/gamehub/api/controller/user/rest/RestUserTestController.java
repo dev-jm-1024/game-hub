ca2c700c1b00958b929ef956ddf2e0de99233b95
dev-jm <develop6013@gmail.com>
2025-07-19 21:40:56 +0900

    RestUserTestController - temp
    RestUserController  리팩토링하기 위해 임시 작업 요소

---
+++ src/main/java/kr/plusb3b/games/gamehub/api/controller/user/rest/RestUserTestController.java
@@ -0,0 +1,38 @@
+package kr.plusb3b.games.gamehub.api.controller.user.rest;
+
+import kr.plusb3b.games.gamehub.domain.user.dto.ChangePasswordDto;
+import kr.plusb3b.games.gamehub.domain.user.repository.UserAuthRepository;
+import kr.plusb3b.games.gamehub.security.AccessControlService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api/v1")
+public class RestUserTestController {
+
+    private AccessControlService access;
+    private UserAuthRepository userAuthRepo;
+
+    public RestUserTestController(AccessControlService access, UserAuthRepository userAuthRepo) {
+        this.access = access;
+        this.userAuthRepo = userAuthRepo;
+    }
+
+
+    @PostMapping("/user/password")
+    public ResponseEntity<?> changePassword(@RequestBody ChangePasswordDto changePasswordDto,
+                                            HttpServletRequest request) {
+
+
+
+
+        return ResponseEntity.status(HttpStatus.OK).body("test");
+    }
+
+
+}

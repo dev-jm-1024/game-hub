defd6735408f536d9251bd30d6c4de6878b0a295
dev-jm <develop6013@gmail.com>
2025-07-27 21:10:14 +0900

    feat: CommentsInteractionServiceImpl
    CommentsInteractionService 인터페이스 구현체 클래스 파일 생성.
    아직 구현하지 않았으므로 추후 구현 예정

---
+++ src/main/java/kr/plusb3b/games/gamehub/application/board/CommentsInteractionServiceImpl.java
@@ -0,0 +1,19 @@
+package kr.plusb3b.games.gamehub.application.board;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentsInteractionServiceImpl {
+
+    //좋아요 누르기
+
+    //좋아요 취소
+
+    //싫어요
+
+    //싫어요 취소
+
+    //신고하기
+
+    //신고하기 취소
+}

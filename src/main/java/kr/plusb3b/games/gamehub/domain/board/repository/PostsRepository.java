8430b3b3b4a8e583130ecaec4dcc6d7d92bdcae7
dev-jm <develop6013@gmail.com>
2025-07-27 21:16:36 +0900

    feat : PostsRepository
    사용하지 않는 메소드 제거
    : findByBoard_BoardId, findByBoard_BoardIdAndPostId

--- src/main/java/kr/plusb3b/games/gamehub/domain/board/repository/PostsRepository.java
+++ src/main/java/kr/plusb3b/games/gamehub/domain/board/repository/PostsRepository.java
@@ -14,12 +14,6 @@ import java.util.Optional;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
-    List<Posts> findByBoard_BoardId(String boardId, Pageable pageable);
-
-    //List로 Posts 데이터 받기
-    //해당 게시판의 아이디와 게시물의 아이디를 입력받아 해당 게시물 데이터 가져오기
-    //예외처리를 보다 쉽게 처리하기 위해 Optional 로 처리
-    Optional<Posts> findByBoard_BoardIdAndPostId(String boardId, Long postId);
 
     //boardId를 입력받아, 해당 게시판의 게시물이 존재하는 지 확인
     List<Posts> findByBoard_BoardId(String boardId);
@@ -46,10 +40,6 @@ public interface PostsRepository extends JpaRepository<Posts, Long> {
     @Query("SELECT p FROM Posts p WHERE p.user.userAuth.authUserId = :authUserId AND p.board.boardId = :boardId")
     Optional<Posts> findPostsByUserAuth(@Param("authUserId") String authUserId,
                                         @Param("boardId") String boardId);
-
-
-    Optional<Posts> findByPostIdAndBoard_BoardIdAndPostAct(Long postId, String boardId, int postAct);
-
 
 
 }

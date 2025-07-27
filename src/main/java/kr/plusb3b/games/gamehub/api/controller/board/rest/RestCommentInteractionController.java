509068bdf8a03ca721bb9e14a992d41d901e68b4
dev-jm <develop6013@gmail.com>
2025-07-27 21:17:59 +0900

    feat: RestCommentInteractionController
    댓글 좋아요, 싫어요 관련 REST API 클래스 파일 생성

---
+++ src/main/java/kr/plusb3b/games/gamehub/api/controller/board/rest/RestCommentInteractionController.java
@@ -0,0 +1,11 @@
+package kr.plusb3b.games.gamehub.api.controller.board.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestCommentInteractionController {
+
+
+
+}

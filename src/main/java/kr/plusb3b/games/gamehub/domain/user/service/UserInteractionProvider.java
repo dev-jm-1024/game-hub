f238d79485f76284b5ee9d10ee9480e7985d06f5
dev-jm <develop6013@gmail.com>
2025-08-02 01:07:08 +0900

    feat: 댓글 반응 조회 기능을 UserInteractionProvider 인터페이스에 추가
    - 게시물 반응 타입 조회 메서드 이름 변경: getUserPostsReactionType
    - 댓글 반응 타입 조회 메서드 추가: getUserCommentsReactionType
    - 게시물과 댓글의 반응 구분 처리를 위한 기반 서비스 인터페이스 확장

--- src/main/java/kr/plusb3b/games/gamehub/domain/user/service/UserInteractionProvider.java
+++ src/main/java/kr/plusb3b/games/gamehub/domain/user/service/UserInteractionProvider.java
@@ -1,12 +1,17 @@
 package kr.plusb3b.games.gamehub.domain.user.service;
 
 import jakarta.servlet.http.HttpServletRequest;
+import kr.plusb3b.games.gamehub.domain.board.entity.Comments;
 import kr.plusb3b.games.gamehub.domain.board.entity.Posts;
 import kr.plusb3b.games.gamehub.domain.user.entity.User;
+import kr.plusb3b.games.gamehub.domain.user.entity.UserCommentsReaction;
 import kr.plusb3b.games.gamehub.domain.user.entity.UserPostsReaction;
 
 public interface UserInteractionProvider {
 
-    //1. 사용자가 어떠한 상태를 눌렀는지?
-    UserPostsReaction.ReactionType getUserReactionType(Posts posts, User user);
+    //1. 사용자가 어떠한 상태를 눌렀는지? - 게시물
+    UserPostsReaction.ReactionType getUserPostsReactionType(Posts posts, User user);
+
+    //2. 사용자가 어떠한 상태를 눌렀는지? - 댓글
+    UserCommentsReaction.ReactionType getUserCommentsReactionType(Comments comments, User user);
 }

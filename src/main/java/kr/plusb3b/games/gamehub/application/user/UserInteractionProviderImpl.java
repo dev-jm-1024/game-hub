e1f15bad2fc5ff7f434cddf4ff6670f5cfd6d50d
dev-jm <develop6013@gmail.com>
2025-08-02 01:08:02 +0900

    feat: UserInteractionProviderImpl에 댓글 반응 조회 기능 추가
    - getUserCommentsReactionType 구현
    - userCommentsReactionRepo 활용하여 댓글에 대한 유저 반응 조회
    - 기존 게시물 반응 조회 기능(getUserPostsReactionType)과 일관된 구조 유지

--- src/main/java/kr/plusb3b/games/gamehub/application/user/UserInteractionProviderImpl.java
+++ src/main/java/kr/plusb3b/games/gamehub/application/user/UserInteractionProviderImpl.java
@@ -1,9 +1,12 @@
 package kr.plusb3b.games.gamehub.application.user;
 
+import kr.plusb3b.games.gamehub.domain.board.entity.Comments;
 import kr.plusb3b.games.gamehub.domain.board.entity.Posts;
 import kr.plusb3b.games.gamehub.domain.board.repository.PostsRepository;
 import kr.plusb3b.games.gamehub.domain.user.entity.User;
+import kr.plusb3b.games.gamehub.domain.user.entity.UserCommentsReaction;
 import kr.plusb3b.games.gamehub.domain.user.entity.UserPostsReaction;
+import kr.plusb3b.games.gamehub.domain.user.repository.UserCommentsReactionRepository;
 import kr.plusb3b.games.gamehub.domain.user.repository.UserPostsReactionRepository;
 import kr.plusb3b.games.gamehub.domain.user.service.UserInteractionProvider;
 import kr.plusb3b.games.gamehub.security.AccessControlService;
@@ -14,23 +17,20 @@ import java.util.Optional;
 @Service
 public class UserInteractionProviderImpl implements UserInteractionProvider {
 
-    private final AccessControlService access;
-    private final UserPostsReactionRepository userReactionRepo;
-    private final PostsRepository postsRepo;
+    private final UserPostsReactionRepository userPostsReactionRepo;
+    private final UserCommentsReactionRepository userCommentsReactionRepo;
 
-    public UserInteractionProviderImpl(AccessControlService access,
-                                       UserPostsReactionRepository userReactionRepo,
-                                       PostsRepository postsRepo) {
-        this.access = access;
-        this.userReactionRepo = userReactionRepo;
-        this.postsRepo = postsRepo;
+
+    public UserInteractionProviderImpl(UserPostsReactionRepository userPostsReactionRepo,
+                                       UserCommentsReactionRepository userCommentsReactionRepo) {
+        this.userPostsReactionRepo = userPostsReactionRepo;
+        this.userCommentsReactionRepo = userCommentsReactionRepo;
     }
 
     @Override
-    public UserPostsReaction.ReactionType getUserReactionType(Posts posts, User user) {
+    public UserPostsReaction.ReactionType getUserPostsReactionType(Posts posts, User user) {
+        Optional<UserPostsReaction> reactionOpt = userPostsReactionRepo.findByUserAndPosts(user, posts);
 
-
-        Optional<UserPostsReaction> reactionOpt = userReactionRepo.findByUserAndPosts(user, posts);
         if(reactionOpt.isPresent()) {
             return reactionOpt.get().getReactionType();
         }else{
@@ -38,5 +38,15 @@ public class UserInteractionProviderImpl implements UserInteractionProvider {
         }
     }
 
+    //댓글 관련 로직 적어야함
+    @Override
+    public UserCommentsReaction.ReactionType getUserCommentsReactionType(Comments comments, User user) {
+        Optional<UserCommentsReaction> reactionOpt = userCommentsReactionRepo.findByUserAndComments(user, comments);
 
+        if(reactionOpt.isPresent()) {
+            return reactionOpt.get().getReactionType();
+        }else{
+            return UserCommentsReaction.ReactionType.NONE;
+        }
+    }
 }

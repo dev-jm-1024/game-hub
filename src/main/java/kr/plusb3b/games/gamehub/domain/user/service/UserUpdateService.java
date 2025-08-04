4182e60e0c40ff0c29fb4919cf3d5db86a295c1f
dev-jm <develop6013@gmail.com>
2025-08-04 09:58:50 +0900

    Update : UserUpdateService
    사용자 프로필 사진 업데이트 메소드 추상화 작업

--- src/main/java/kr/plusb3b/games/gamehub/domain/user/service/UserUpdateService.java
+++ src/main/java/kr/plusb3b/games/gamehub/domain/user/service/UserUpdateService.java
@@ -1,11 +1,18 @@
 package kr.plusb3b.games.gamehub.domain.user.service;
 
 import kr.plusb3b.games.gamehub.domain.user.dto.RequestUserUpdateDto;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface UserUpdateService {
 
     void updateUserProfile(Long mbId, RequestUserUpdateDto userUpdateDto);
-
     void updateUserPassword(Long mbId, String currentPassword, String newPassword);
+
+    // 새로 추가할 메서드
+    String uploadAndUpdateProfileImage(Long mbId, MultipartFile file);
 
 }

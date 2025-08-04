7a1a82c5d0956d283605ccaac1d8c89133b0051c
dev-jm <develop6013@gmail.com>
2025-08-04 10:03:17 +0900

    Update : UserInteractionProviderImpl
    1. 사용자가 게시물을 신고하였는 지 확인하는 메소드
    2. 사용자가 댓글을 신고하였는 지 확인하는 메소드
    각각 인터페이스에 명시

--- src/main/java/kr/plusb3b/games/gamehub/domain/user/service/UserInteractionProvider.java
+++ src/main/java/kr/plusb3b/games/gamehub/domain/user/service/UserInteractionProvider.java
@@ -12,6 +12,14 @@ public interface UserInteractionProvider {
     //1. 사용자가 어떠한 상태를 눌렀는지? - 게시물
     UserPostsReaction.ReactionType getUserPostsReactionType(Posts posts, User user);
 
+    //1.1 사용자가 해당 게시물을 신고하였는가? - 게시물
+    boolean getUserPostsReportReactionType(Posts posts, User user);
+
     //2. 사용자가 어떠한 상태를 눌렀는지? - 댓글
     UserCommentsReaction.ReactionType getUserCommentsReactionType(Comments comments, User user);
+
+    //2.1 사용자가 해당 게시물을 신고하였는가? - 댓글
+    boolean getUserCommentsReportReactionType(Comments comments, User user);
+
+
 }

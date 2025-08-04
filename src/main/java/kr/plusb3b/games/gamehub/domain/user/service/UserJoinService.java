8a5ab96f09630a8e3acb55146d92de1d4e95e02b
dev-jm <develop6013@gmail.com>
2025-08-04 10:02:02 +0900

    update : UserJoinService
    signupUser() 메소드에 MultipartFile 리스트 추가.
    이 파라미터를 통해 사진 파일을 넘겨 받고 업로드 메소드 호출하여 사진 업로드 실행

--- src/main/java/kr/plusb3b/games/gamehub/domain/user/service/UserJoinService.java
+++ src/main/java/kr/plusb3b/games/gamehub/domain/user/service/UserJoinService.java
@@ -4,12 +4,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import kr.plusb3b.games.gamehub.domain.user.entity.User;
 import kr.plusb3b.games.gamehub.domain.user.dto.UserSignupDto;
 import kr.plusb3b.games.gamehub.domain.user.vo.UserSignupVO;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface UserJoinService {
 
     //객체 조립 후 저장
     //사용자 입력 데이터 + 추가 데이터
-    void signupUser(UserSignupDto userSignupDto, UserSignupVO usv);
+    void signupUser(UserSignupDto userSignupDto, UserSignupVO usv, List<MultipartFile> files);
 
     //비로그인 상태인지 검사
     boolean isLogin(HttpServletRequest request);

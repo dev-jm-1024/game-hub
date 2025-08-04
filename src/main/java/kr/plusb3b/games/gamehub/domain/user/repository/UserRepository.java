6c6afa6d41d429d2398279dc0f8e2071dcac5474
dev-jm <develop6013@gmail.com>
2025-08-04 09:59:47 +0900

    Update : UserRepository
    1. 사용하지 않는 메소드 제거

--- src/main/java/kr/plusb3b/games/gamehub/domain/user/repository/UserRepository.java
+++ src/main/java/kr/plusb3b/games/gamehub/domain/user/repository/UserRepository.java
@@ -12,26 +12,10 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    //뭔가 안되어지고 있음
-    @Query("SELECT u FROM User u WHERE u.userAuth.authUserId = :authUserId")
-    Optional<User> findByUserAuth_AuthUserId(@Param("authUserId") String authUserId);
-
-    //회원계정 비활성화 -- 탈퇴
+    // 사용자 신고횟수 증가
+    @Modifying(clearAutomatically = true)
     @Transactional
-    @Modifying
-    @Query("UPDATE User SET mbAct = 0 WHERE userAuth.authUserId = :authUserId AND mbAct = 1")
-    int updateMbActByAuthUserId(@Param("authUserId") String authUserId);
-
-    //프로필 업데이트 메소드
-    @Transactional
-    @Modifying
-    @Query("UPDATE User u SET u.mbNickname = :mbNickName, u.mbProfileUrl = :mbProfileUrl, u.mbStatusMessage = :mbStatusMessage WHERE u.mbId = :mbId")
-    int updateUserByMbId(
-            @Param("mbNickName") String mbNickName,
-            @Param("mbProfileUrl") String mbProfileUrl,
-            @Param("mbStatusMessage") String mbStatusMessage,
-            @Param("mbId") Long mbId
-    );
-
+    @Query("UPDATE User u SET u.mbReportCnt = u.mbReportCnt + 1 WHERE u.mbId = :mbId")
+    int incrementReportCountByMbId(@Param("mbId") Long mbId);
 
 }

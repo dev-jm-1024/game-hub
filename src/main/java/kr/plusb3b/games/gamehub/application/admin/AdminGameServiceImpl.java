494ccf5dba9cd5513c5bb49bd8fc46cc234b1d14
dev-jm <develop6013@gmail.com>
2025-08-09 21:00:50 +0900

    feat(admin): AdminGameServiceImpl 기본 구현 추가 - approveGame, rejectGame 메서드 초기 구현

---
+++ src/main/java/kr/plusb3b/games/gamehub/application/admin/AdminGameServiceImpl.java
@@ -0,0 +1,16 @@
+package kr.plusb3b.games.gamehub.application.admin;
+
+import kr.plusb3b.games.gamehub.domain.admin.service.AdminGameService;
+
+public class AdminGameServiceImpl implements AdminGameService {
+
+    @Override
+    public boolean approveGame(Long gameId) {
+        return true;
+    }
+
+    @Override
+    public boolean rejectGame(Long gameId) {
+        return false;
+    }
+}

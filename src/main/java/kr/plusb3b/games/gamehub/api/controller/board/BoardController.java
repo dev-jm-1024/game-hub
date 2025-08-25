a4217f281e77d9fe93bf510b68d7468408cef760
dev-jm <develop6013@gmail.com>
2025-08-25 15:37:22 +0900

    불필요 주석 제거

--- src/main/java/kr/plusb3b/games/gamehub/api/controller/board/BoardController.java
+++ src/main/java/kr/plusb3b/games/gamehub/api/controller/board/BoardController.java
@@ -23,11 +23,6 @@ public class BoardController {
     private final PostsService postsService;
     private final BoardRepository boardRepo;
     private final GameMetadataService gameMetadataService;
-    //private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(BoardController.class);4
-
-//    @Value("${app.max.boardSize}")
-//    String maxBoardSizeString;
-//    int maxBoardSize = Integer.parseInt(maxBoardSizeString);
 
 
     public BoardController(BoardService boardService, PostsService postsService, BoardRepository boardRepo,

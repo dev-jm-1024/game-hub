d04e3cc1d9db84fa075931b6cb96aca5604b9112
dev-jm <develop6013@gmail.com>
2025-08-25 15:34:57 +0900

    feat: 관리자 게시판 관련 경로 설정용 AdminBoardConfig 추가
    - application.yml의 admin 관련 설정값 주입
      - admin.api.path
      - admin.board.activate / deactivate / rename / create
    - 관리자 게시판 기능별 URL 경로를 Map으로 제공하는 getAdminBoardPaths() 메소드 구현

--- src/main/java/kr/plusb3b/games/gamehub/application/admin/AdminBoardConfig.java
+++ src/main/java/kr/plusb3b/games/gamehub/application/admin/AdminBoardConfig.java
@@ -1,7 +1,37 @@
 package kr.plusb3b.games.gamehub.application.admin;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class AdminBoardConfig {
+
+    @Value("${admin.api.path}")
+    private String basePath;
+
+    @Value("${admin.board.activate}")
+    private String activatePath;
+
+    @Value("${admin.board.deactivate}")
+    private String deactivatePate;
+
+    @Value("${admin.board.rename}")
+    private String renamePath;
+
+    @Value("${admin.board.create}")
+    private String createPath;
+
+
+    public Map<String, String> getAdminBoardPaths() {
+        return Map.ofEntries(
+                Map.entry("activate", basePath+activatePath),
+                Map.entry("deactivate", basePath+deactivatePate),
+                Map.entry("rename", basePath+renamePath),
+                Map.entry("makeBoard", basePath+createPath)
+        );
+    }
+
 }

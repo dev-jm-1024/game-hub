db3c93da0d65da2b8f429041797354d808d69f9a
dev-jm <develop6013@gmail.com>
2025-08-31 14:41:44 +0900

    feat: PostGamesDetailVmService 구현체 추가
    - 인증된 사용자 정보를 기반으로 게임 상세 뷰 모델(PostGamesDetailVM) 생성
    - 관리자 여부, 사용자 닉네임 및 프로필 이미지 처리 로직 포함
    - GameMetadataService를 통해 게임 상세 정보(GameDetailDto) 주입

---
+++ src/main/java/kr/plusb3b/games/gamehub/application/board/viewmodel/PostGamesDetailVmServiceImpl.java
@@ -0,0 +1,43 @@
+package kr.plusb3b.games.gamehub.application.board.viewmodel;
+
+import jakarta.servlet.http.HttpServletRequest;
+import kr.plusb3b.games.gamehub.domain.board.entity.Board;
+import kr.plusb3b.games.gamehub.domain.board.service.viewmodel.PostGamesDetailVmService;
+import kr.plusb3b.games.gamehub.domain.game.dto.GameDetailDto;
+import kr.plusb3b.games.gamehub.domain.game.service.GameMetadataService;
+import kr.plusb3b.games.gamehub.domain.user.entity.User;
+import kr.plusb3b.games.gamehub.security.AccessControlService;
+import kr.plusb3b.games.gamehub.view.board.PostDetailVM;
+import kr.plusb3b.games.gamehub.view.board.PostGamesDetailVM;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostGamesDetailVmServiceImpl implements PostGamesDetailVmService {
+
+    private final GameMetadataService gameMetadataService;
+    private final AccessControlService access;
+
+    public PostGamesDetailVmServiceImpl(GameMetadataService gameMetadataService, AccessControlService access) {
+        this.gameMetadataService = gameMetadataService;
+        this.access = access;
+    }
+
+    @Override
+    public PostGamesDetailVM getPostGamesDetailVm(HttpServletRequest request, String boardId, Long gameId) {
+
+        User user = access.getAuthenticatedUser(request);
+        boolean isAdmin = user.getMbRole() == User.Role.ROLE_ADMIN;
+
+        final String replaceProfileUrl = "https://www.shutterstock.com/image-vector/vector-flat-illustration-grayscale-avatar-600nw-2281862025.jpg";
+        GameDetailDto result = gameMetadataService.getGameDetail(boardId, gameId);
+
+        return new PostGamesDetailVM(
+                user,
+                isAdmin,
+                user.getMbNickname(),        // 올바른 닉네임
+                user.getMbProfileUrl() != null ? user.getMbProfileUrl() : replaceProfileUrl, // 프로필 URL 로직
+                replaceProfileUrl,           // 기본 프로필 URL
+                result
+        );
+    }
+}

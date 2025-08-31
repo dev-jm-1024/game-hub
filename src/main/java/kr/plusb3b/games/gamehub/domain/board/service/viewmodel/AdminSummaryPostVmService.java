788422e69802e6a27e4cb1a4c4258a0c38aa7e8b
dev-jm <develop6013@gmail.com>
2025-08-31 14:40:29 +0900

    feat: 관리자 게시글 요약 조회용 AdminSummaryPostVmService 인터페이스 정의
    - 게시판 ID를 기반으로 AdminSummaryPostVM 리스트 반환 메서드 선언
    - 게시판별 게시글 요약 정보(제목, 반응 수 등) 제공을 위한 서비스 계층 설계

---
+++ src/main/java/kr/plusb3b/games/gamehub/domain/board/service/viewmodel/AdminSummaryPostVmService.java
@@ -0,0 +1,10 @@
+package kr.plusb3b.games.gamehub.domain.board.service.viewmodel;
+
+import kr.plusb3b.games.gamehub.view.admin.AdminSummaryPostVM;
+
+import java.util.List;
+
+public interface AdminSummaryPostVmService {
+
+    List<AdminSummaryPostVM> getAdminSummaryPostVm(String boardId);
+}

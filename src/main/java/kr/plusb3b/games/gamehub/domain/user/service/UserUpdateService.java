afafd9503bdc3dbf57402432e0e0483d2c275c99
dev-jm-1024 <develop6013@gmail.com>
2025-09-20 00:59:26 +0900

    refactor(user): UserUpdateService 메서드 시그니처 개선
    - updateUserPassword 메서드에서 개별 파라미터 제거 후 ChangePasswordDto 객체 기반으로 변경
    - 관련 DTO 활용으로 유지보수성과 확장성 강화

--- src/main/java/kr/plusb3b/games/gamehub/domain/user/service/UserUpdateService.java
+++ src/main/java/kr/plusb3b/games/gamehub/domain/user/service/UserUpdateService.java
@@ -1,5 +1,6 @@
 package kr.plusb3b.games.gamehub.domain.user.service;
 
+import kr.plusb3b.games.gamehub.domain.user.dto.ChangePasswordDto;
 import kr.plusb3b.games.gamehub.domain.user.dto.RequestUserUpdateDto;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -10,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 public interface UserUpdateService {
 
     void updateUserProfile(Long mbId, RequestUserUpdateDto userUpdateDto);
-    void updateUserPassword(Long mbId, String currentPassword, String newPassword);
+    void updateUserPassword(Long mbId, ChangePasswordDto dto);
 
     // 새로 추가할 메서드
     String uploadAndUpdateProfileImage(Long mbId, MultipartFile file);

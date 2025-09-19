b9b31ecd2fb274950b135e558b2f299bd55d6647
dev-jm-1024 <develop6013@gmail.com>
2025-09-20 00:21:21 +0900

    refactor: 유효성 검사 추가

--- src/main/java/kr/plusb3b/games/gamehub/domain/user/dto/ChangePasswordDto.java
+++ src/main/java/kr/plusb3b/games/gamehub/domain/user/dto/ChangePasswordDto.java
@@ -1,5 +1,6 @@
 package kr.plusb3b.games.gamehub.domain.user.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,7 +8,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ChangePasswordDto {
 
+    @NotNull(message = "기존 비밀번호를 입력하세요.")
     private String oldPassword;
+
+    @NotNull(message = "현재 비밀번호를 입력하세요.")
     private String newPassword;
 
 }

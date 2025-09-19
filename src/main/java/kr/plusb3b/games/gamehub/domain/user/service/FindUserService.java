9ea259bff7c6e27c781c0a53a511d950b42c0ddd
dev-jm-1024 <develop6013@gmail.com>
2025-09-20 00:26:23 +0900

    feat(user): FindUserService 인터페이스 추가
    - findUserAuth(FindUserDto dto) 메서드 정의
    - 사용자 인증 정보 조회 시 Optional<FindUserDto> 반환
    - 구현체에서 DTO 기반 조회 로직을 확장 가능하도록 설계

---
+++ src/main/java/kr/plusb3b/games/gamehub/domain/user/service/FindUserService.java
@@ -0,0 +1,11 @@
+package kr.plusb3b.games.gamehub.domain.user.service;
+
+import kr.plusb3b.games.gamehub.domain.user.dto.FindUserDto;
+
+import java.util.Optional;
+
+public interface FindUserService {
+
+    Optional<FindUserDto> findUserAuth(FindUserDto dto);
+
+}

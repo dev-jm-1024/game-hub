eb569d9a7fbb937194aa50fcd6be0d2a40bd13f8
dev-jm-1024 <develop6013@gmail.com>
2025-09-20 00:54:54 +0900

    feat(user): UserLoginInfoRepository에 사용자별 로그인 이력 조회 메서드 추가
    - UserLoginInfoRepository에 getUserLoginInfoByUser_MbId(Long userMbId) 메서드 추가
    - 특정 사용자(mbId) 기준으로 로그인 이력 리스트 조회 가능

--- src/main/java/kr/plusb3b/games/gamehub/domain/user/repository/UserLoginInfoRepository.java
+++ src/main/java/kr/plusb3b/games/gamehub/domain/user/repository/UserLoginInfoRepository.java
@@ -3,5 +3,8 @@ package kr.plusb3b.games.gamehub.domain.user.repository;
 import kr.plusb3b.games.gamehub.domain.user.entity.UserLoginInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserLoginInfoRepository extends JpaRepository<UserLoginInfo, Long> {
+    List<UserLoginInfo> getUserLoginInfoByUser_MbId(Long userMbId);
 }

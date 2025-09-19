326551ea7678fb190e5161b8dfa8718d0d6fcb3e
dev-jm-1024 <develop6013@gmail.com>
2025-09-20 00:22:25 +0900

    refactor(board): CommentService 인터페이스 메서드 시그니처 단순화
    - createComment() 메서드에서 CreateCommentsVO 제거, RequestCommentDto와 User만 전달
    - 불필요한 VO 중복 제거로 서비스 계층 책임 명확화
    - 댓글 생성 시 DTO 중심으로 데이터 처리

--- src/main/java/kr/plusb3b/games/gamehub/domain/board/service/CommentService.java
+++ src/main/java/kr/plusb3b/games/gamehub/domain/board/service/CommentService.java
@@ -2,14 +2,13 @@ package kr.plusb3b.games.gamehub.domain.board.service;
 
 import kr.plusb3b.games.gamehub.domain.board.dto.RequestCommentDto;
 import kr.plusb3b.games.gamehub.domain.board.entity.Comments;
-import kr.plusb3b.games.gamehub.domain.board.vo.CreateCommentsVO;
 import kr.plusb3b.games.gamehub.domain.user.entity.User;
 
 import java.util.List;
 
 public interface CommentService {
 
-    Comments createComment(CreateCommentsVO cvo, RequestCommentDto requestCommentDto, User user);
+    Comments createComment(RequestCommentDto requestCommentDto, User user);
 
     List<Comments> getComments(String boardId, Long postId);
 
